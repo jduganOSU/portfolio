46a3d9ee8d63f405681e7bcec4768f1e1e551934
jackdugan <duganjon@oregonstate.edu>
2023-08-23 21:12:25 -0400

    8/23

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const organRegistryRouter = require('./organRegistry/app');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Handlebars setup
 const hbs = exphbs.create({ extname: '.hbs'});
